b7c49cb2a6eeb83ebe2b291bc45a01ece6472fd0
Lucas Rodrigues <lr579063@gmail.com>
2021-10-15 13:48:34 -0300

    projeto final

--- parte.js
+++ parte.js
@@ -1,3 +1,6 @@
 document.querySelector('#dropdown-menu').addEventListener('click', () => {
     document.querySelector('.menu-mobile').style.width = '200px';
 });
+document.querySelector('#botao-fechar').addEventListener('click', () => {
+    document.querySelector('.menu-mobile').style.width = '0';  
+});
